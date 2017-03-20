0aca27e817380da56952b0cc3ee4cae7f127e0f8
deltamaze <wcpooley@gmail.com>
2017-03-19 23:43:46 -0500

    init

---
+++ p5-app/snake.js
@@ -0,0 +1,67 @@
+
+function Snake() {
+    this.x = 0;
+    this.y = 0;
+    this.xspeed = 0;
+    this.yspeed = 0;
+    this.total = 0;
+    this.tail = [];
+
+    this.update = function () {
+        
+        if(this.total=== this.tail.length){
+            for (var i = 0; i < this.tail.length; i++) {
+                this.tail[i] = this.tail[i+1];
+        }}
+        else
+        {
+            console.log(this.total);
+            console.log(this.tail);
+
+        }
+        console.log(this.total);
+        if(this.total>0)
+        {
+            this.tail[this.total-1] = createVector(this.x,this.y)
+
+        }
+        
+        //console.log(this.x);
+        this.x = this.x + this.xspeed * scl;
+        this.y = this.y + this.yspeed * scl;
+
+        this.x = constrain(this.x, 0, width - scl)
+        this.y = constrain(this.y, 0, height - scl)
+
+        
+
+    }
+
+    this.show = function () {
+        fill(255);
+        for (var i = 0; i < this.total; i++) {
+            rect(this.tail[i].x, this.tail[i].y, scl, scl);
+        }
+        rect(this.x, this.y, scl, scl);
+    }
+
+    this.dir = function (x, y) {
+        //   if(this.xspeed !=x || this.yspeed !=y){
+        //       console.log(this.x);
+        //       console.log(this.y);
+        //   }
+        this.xspeed = x;
+        this.yspeed = y;
+    }
+
+    this.eat = function (food) {
+        var d = dist(this.x, this.y, food.x, food.y);
+        if (d < 1) {
+            this.total++;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
